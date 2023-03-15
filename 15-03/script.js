250efd5cc8cd5b3f7dfa05c48bc2a5cb5d58d839
BegimaI <babdrahmanova11@gmail.com>
2023-03-15 11:08:55 +0600

    completed the task

---
+++ 15-03/script.js
@@ -0,0 +1,17 @@
+//String
+// let userName = 'Begimai';
+// let companyName = "Mega";
+// let lastName =  ${companyName} Abdrakhmanova ${userName}
+// console.log(lastName)
+
+let name = 'Бегимай';
+let hello = `Привет, ${name}!`;
+console.log(hello)
+
+let age = '17';
+let myAge = ` Мне ${age}лет!`;
+console.log(myAge)
+
+let str1 = 'САЛАМ,';
+let str2 = ` ${str1} БИШКЕК!`;
+console.log(str2)
