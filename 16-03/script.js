9764a7ea484d7b268f8005773b751fe2347415c7
BegimaI <babdrahmanova11@gmail.com>
2023-03-16 10:51:41 +0600

    completed the task

---
+++ 16-03/script.js
@@ -0,0 +1,21 @@
+/* first task */
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+/* second task */
+let hour = 10;
+let minute = 30;
+let second = 20;
+// console.log(hour,minute,second);
+let time = `${hour}:${minute}:${second}`;
+console.log(time);
+
+/* third task */
+var num = 10;
+num = num++;
+num = ++num;
+console.log(num);
