5dc8f72ddf58aadbfaf5bc7ab9c8a4e7f6dd94bd
BegimaI <babdrahmanova11@gmail.com>
2023-03-17 11:31:56 +0600

    completed second task

--- 17-03/script.js
+++ 17-03/script.js
@@ -25,3 +25,29 @@ const storageSize = 226;
 
 const check4 = laptop > '' &&  storageSize == 226 ;
 console.log(check4);
+
+/* second task */
+//1) ex
+let num = 10;
+num++;
+num++;
+num--;
+console.log(num); //11
+
+//2) ex
+let num2 = 47;
+num2 += 7;
+num2 -= 18;
+num2 *= 10;
+num2 /= 15;
+console.log(num2); //24
+
+//3) ex
+let num3 = 1;
+num3 += 12;
+num3 -= 14;
+num3 *= 5;
+num3 /= 7;
+num3++;
+num3--;
+console.log(num3); //-0.7142857142857143
